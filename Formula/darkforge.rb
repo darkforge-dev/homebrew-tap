2ddd02cb544b809508b93b494b905a0dc3d6173c
Narayan Sainaney <narayan@sainaney.com>
2026-05-24 15:49:42 -0700

    Update darkforge to vv1.0.41m6

--- Formula/darkforge.rb
+++ Formula/darkforge.rb
@@ -1,28 +1,28 @@
 class Darkforge < Formula
   desc "AI-powered development environment"
   homepage "https://darkforge.dev"
-  version "v1.0.41m5"
+  version "v1.0.41m6"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m5/darkforge-darwin-arm64"
-      sha256 "fd5160d9d0c09651e0fbf5c7a09849f4e9c1afdb186a1fd28b1cb5870b24a85d"
+      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m6/darkforge-darwin-arm64"
+      sha256 "340c413e122349739472825677a4089c411f92d21cf6fa502782f21a18279fc1"
     end
     on_intel do
-      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m5/darkforge-darwin-amd64"
-      sha256 "3ed3565ceb3d5f9fc91308411fb28fbb28d7c0d342e6adc5d254e1365a4b9ab5"
+      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m6/darkforge-darwin-amd64"
+      sha256 "8de424625c4bd3256536cb6130cc9333eb2b17572e4e239e85452b70cfaf6da4"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m5/darkforge-linux-arm64"
-      sha256 "cefc832b97a2b26be658e7a2cb3e82347f471a44cfde71eb314a824b118c2dea"
+      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m6/darkforge-linux-arm64"
+      sha256 "541ebc72ae575941d92a918cb24bdaacd61e4414b0715c4eef73a366644be131"
     end
     on_intel do
-      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m5/darkforge-linux-amd64"
-      sha256 "3b6b8806a1db98052f4d9908cd9c71813c7a0e998d1b0a85199bbd4c9a5c16fb"
+      url "https://github.com/darkforge-dev/darkforge/releases/download/vv1.0.41m6/darkforge-linux-amd64"
+      sha256 "0120f5f9235959cb8c740a04e0274b862dff7be0ab9c6a55acf99383b67d6dad"
     end
   end
 
